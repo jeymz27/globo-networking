a402aab33aa7c4e3ade7daf4927bfead7fcc7199
James Potluri <james.potluri@gmail.com>
2025-01-20 23:25:44 -0500

    Fixed formatting

--- variables.tf
+++ variables.tf
@@ -15,10 +15,10 @@ variable "prefix" {
 }
 
 variable "environment" {
-  type = string
+  type        = string
   description = "(Optional) Environment for all resources"
-  default = "development"
-  
+  default     = "development"
+
 }
 
 variable "cidr_block" {
